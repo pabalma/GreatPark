675f0cd49eb6d1dae3201948fe69f746e9c6d17d
tjoeun707-12 <tjoeun707-12@DESKTOP-HLM3RQK.mshome.net>
2022-04-21 15:31:23 +0900

    04-21

--- greatpark/src/main/java/com/myproject/greatpark/model/cart/CartDAO.java
+++
@@ -1,7 +0,0 @@
-package com.myproject.greatpark.model.cart;
-
-import java.util.List;
-
-public interface CartDAO {
-	List<CartDTO> list(String userid);
-}
--- greatpark/src/main/java/com/myproject/greatpark/model/product/ProductDAO.java
+++
@@ -1,7 +0,0 @@
-package com.myproject.greatpark.model.product;
-
-import java.util.List;
-
-public interface ProductDAO {
-	List<ProductDTO> list();
-}
